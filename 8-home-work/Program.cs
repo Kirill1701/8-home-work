d1f0647e2353726ca9f459f1eab80cb317f2277c
Kirill Vasilenko <kirvasilenko17@icloud.com>
2023-05-28 22:02:26 +1000

    solution-8.1

--- 8-home-work/Program.cs
+++ 8-home-work/Program.cs
@@ -9,8 +9,37 @@ namespace _8_home_work
 {
     internal class Program
     {
+        static void NumbersAdd(List<int> numbers)
+        {
+            Random random = new Random();
+
+            for (int number = 0; number < 100; number++)
+                numbers.Add(random.Next(101));
+            Console.WriteLine("Выполнен NumbersAdd");
+        }
+
+        static void Print(List<int> numbers)
+        {
+            foreach (int number in numbers)
+                Console.WriteLine(number);
+            Console.WriteLine("Выполнен Print");
+        }
+
+        static void Remove(List<int> numbers)
+        {
+            numbers.RemoveAll(number => number > 25 && number < 50);
+            Console.WriteLine("Выполнен Remove");
+        }
+
         static void Main(string[] args)
         {
+            List<int> numbers = new List<int>();
+
+            NumbersAdd(numbers);
+            Print(numbers);
+            Remove(numbers);
+            Print(numbers);
+            Console.ReadKey();
         }
     }
 }
